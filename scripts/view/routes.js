37502aadf2abb6283930a61e8d06d865d55956ea
Andrew Peacock <todoville@gmail.com>
2018-05-31 20:52:24 -0700

    finishing up May 30th

--- scripts/view/routes.js
+++ scripts/view/routes.js
@@ -3,7 +3,7 @@
 page('/', ctx => app.weatherView.initIndexPage());
 page('/weather/start', ctx => app.weatherView.initLocationSearch());
 page('/about-us', () => app.weatherView.initAboutUsPage());
-page('/searchhistory')
+page('/searchhistory', () => app.Weather.fetchAll(app.weatherView.initSearchHistoryPage));
 
 
 
