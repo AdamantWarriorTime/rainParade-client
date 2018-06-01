6d1500c3f87cdc507aaa2be043c13b130d1391df
MARIO NISHIO <mario.nishio@gmail.com>
2018-06-01 15:43:40 -0700

    page js keroku path fix

--- scripts/view/routes.js
+++ scripts/view/routes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-page('/', ctx => app.weatherView.initIndexPage());
+page('/rainParade-client', ctx => app.weatherView.initIndexPage());
 page('/weather/start', ctx => app.weatherView.initLocationSearch());
 page('/about-us', () => app.weatherView.initAboutUsPage());
 page('/searchhistory', () => app.Weather.fetchAll(app.weatherView.initSearchHistoryPage));
