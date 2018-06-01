5d4bae48a4b684a0aa5ef1bb92930a04f9d2b887
MARIO NISHIO <mario.nishio@gmail.com>
2018-06-01 09:10:02 -0700

    Try again was not working -fixed

--- scripts/view/routes.js
+++ scripts/view/routes.js
@@ -2,21 +2,8 @@
 
 page('/', ctx => app.weatherView.initIndexPage());
 page('/weather/start', ctx => app.weatherView.initLocationSearch());
-
 page('/about-us', () => app.weatherView.initAboutUsPage());
-
-page('/weather/results', ctx => app.weatherView.initResultsPage());
-
-
-
-
-
-
-
-
-
-
-
+// page('/weather/results', ctx => app.weatherView.initResultsPage());
 page('/show', ctx => app.weatherView.initResultPage());
 
 
